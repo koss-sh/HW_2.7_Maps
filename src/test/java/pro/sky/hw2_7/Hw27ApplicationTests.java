d464b8a007c9a5973234e6f801efefdea736c339
koss-sh <shilinskii22@gmail.com>
2024-02-18 14:57:28 +0300

    hw done

---
+++ src/test/java/pro/sky/hw2_7/Hw27ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.hw2_7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw27ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
